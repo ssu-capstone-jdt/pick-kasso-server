0059eb639b0762916935616b5ab2331f5aab719d
iiqcov <iiqcov@gmail.com>
2024-03-27 15:41:11 +0900

    initial commit

---
+++ src/main/java/com/pickkasso/PickkassoApplication.java
@@ -0,0 +1,13 @@
+package com.pickkasso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickkassoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickkassoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickkasso/PickkassoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickkasso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickkassoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
