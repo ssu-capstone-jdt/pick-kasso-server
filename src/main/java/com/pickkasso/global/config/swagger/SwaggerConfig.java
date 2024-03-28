fb307519b01df1679d2e7fafebb382f9d704ee5f
Jisun You <154600308+iiqcov@users.noreply.github.com>
2024-03-28 22:36:53 +0900

    ⚙️ chore : swagger 세팅 (#4)
    * chore : spotless pre-commit 설정 제거
    
    * chore : swagger 세팅 추가

---
+++ src/main/java/com/pickkasso/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.pickkasso.global.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI SSUPlectorAPI() {
+        Info info =
+                new Info()
+                        .title("Pick kasso API")
+                        .description("Pick kasso API 명세서")
+                        .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components =
+                new Components()
+                        .addSecuritySchemes(
+                                jwtSchemeName,
+                                new SecurityScheme()
+                                        .name(jwtSchemeName)
+                                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
