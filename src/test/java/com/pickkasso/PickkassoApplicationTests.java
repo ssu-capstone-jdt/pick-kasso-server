ff99f4efda982f525f36aa073222d3bf254c3065
iiqcov <iiqcov@gmail.com>
2024-03-28 11:59:04 +0900

    chore : spotless 플러그인 추가

--- src/test/java/com/pickkasso/PickkassoApplicationTests.java
+++ src/test/java/com/pickkasso/PickkassoApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PickkassoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
