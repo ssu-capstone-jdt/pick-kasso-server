5ee8b0a3a18e0be08fd5deb1154606cd2f3d619b
Jisun You <154600308+iiqcov@users.noreply.github.com>
2024-03-29 21:15:57 +0900

    feat : 커스텀 예외 및 에러 코드/응답 구현 (#10)
    * chore : 프로젝트 구조 설정
    
    * feat : 에러 코드 구현
    
    * chore : spotless 제거
    
    * feat : 커스텀 예외 구현
    
    * feat : 예외 응답 구현
    
    * feat : 예외 핸들러 구현

---
+++ src/main/java/com/pickkasso/global/error/exception/ErrorCode.java
@@ -0,0 +1,16 @@
+package com.pickkasso.global.error.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    SAMPLE_ERROR(HttpStatus.BAD_REQUEST, "Sample Error Message"),
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+}
