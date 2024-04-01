eb24c335760a6ff1c57af13f614c34da60cc20a7
iiqcov <iiqcov@gmail.com>
2024-04-01 17:12:19 +0900

    feat : Member 도메인 생성

---
+++ src/main/java/com/pickkasso/domain/member/domain/Member.java
@@ -0,0 +1,43 @@
+package com.pickkasso.domain.member.domain;
+
+import com.pickkasso.domain.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "sns_id")
+    private Long snsId;
+
+    @Column(length = 10)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private SnsPlatform snsPlatform;
+
+    @Builder
+    private Member(Long snsId, String nickname, SnsPlatform snsPlatform) {
+        this.snsId = snsId;
+        this.nickname = nickname;
+        this.snsPlatform = snsPlatform;
+    }
+
+    public static Member createMember(Long snsId, String nickname, SnsPlatform snsPlatform){
+        return Member.builder()
+                .snsId(snsId)
+                .nickname(nickname)
+                .snsPlatform(snsPlatform)
+                .build();
+    }
+}
