95b6aa84a801f0032149b834c8bed277117b62cc
Jisun You <154600308+iiqcov@users.noreply.github.com>
2024-05-06 11:07:07 +0900

    feat : security 기본 설정 (#19)
    * feat : spring security 의존성 추가
    
    * feat : security 설정
    
    * fix : member repository 수정

--- src/main/java/com/pickkasso/domain/member/dao/MemberRepository.java
+++ src/main/java/com/pickkasso/domain/member/dao/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.pickkasso.domain.member.dao;
 
-import java.lang.reflect.Member;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.pickkasso.domain.member.domain.Member;
+
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {}
---
+++ src/main/java/com/pickkasso/global/config/security/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.pickkasso.global.config.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    /*
+     * 임시로 모든 URL 허용
+     * */
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        defaultFilterChain(http);
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("**").permitAll());
+        return http.build();
+    }
+
+    private void defaultFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
