4edd1f051db656cdaa147359fb28126005ae5dd5
iiqcov <iiqcov@gmail.com>
2024-05-12 16:46:36 +0900

    feat : 토큰 검증 및 사용 구현

---
+++ src/main/java/com/pickkasso/domain/auth/dto/AccessTokenDto.java
@@ -0,0 +1,13 @@
+package com.pickkasso.domain.auth.dto;
+
+import com.pickkasso.domain.member.domain.MemberRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AccessTokenDto {
+    private Long memberId;
+    private MemberRole memberRole;
+    private String token;
+}
