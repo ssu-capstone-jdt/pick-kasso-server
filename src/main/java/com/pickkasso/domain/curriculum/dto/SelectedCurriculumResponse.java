153e657a2e990e69d92e8c96226c163eac3c4a03
Nevaeh0701 <Eunsoo071@gmail.com>
2024-05-13 12:11:08 +0900

    fix : round 수정

--- src/main/java/com/pickkasso/domain/curriculum/dto/SelectedCurriculumResponse.java
+++ src/main/java/com/pickkasso/domain/curriculum/dto/SelectedCurriculumResponse.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import com.pickkasso.domain.round.dto.RoundResponse;
 import com.pickkasso.domain.userRound.dto.UserRoundResponse;
+import lombok.Getter;
 
+@Getter
 public class SelectedCurriculumResponse {
     private CurriculumResponse curriculumResponse;
     private List<RoundResponse> roundResponse;
