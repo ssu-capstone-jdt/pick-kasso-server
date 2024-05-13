f1264e8b84c590983b07d13d56ff09f0b9eac161
iiqcov <iiqcov@gmail.com>
2024-05-13 16:32:04 +0900

    fix : 에러코드 추가

--- src/main/java/com/pickkasso/global/error/exception/ErrorCode.java
+++ src/main/java/com/pickkasso/global/error/exception/ErrorCode.java
@@ -13,6 +13,9 @@ public enum ErrorCode {
     ID_TOKEN_VERIFICATION_FAILED(HttpStatus.UNAUTHORIZED, "ID 토큰 검증에 실패했습니다."),
     EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 JWT 토큰입니다."),
 
+    AUTH_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "시큐리티 인증 정보를 찾을 수 없습니다."),
+
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회원을 찾을 수 없습니다."),
     ;
 
     private final HttpStatus status;
