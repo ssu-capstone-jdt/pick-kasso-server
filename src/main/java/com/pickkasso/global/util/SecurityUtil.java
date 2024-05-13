26c74cc5022ee002fa0ef4c823e734a65aa1d6e9
Jisun You <154600308+iiqcov@users.noreply.github.com>
2024-05-13 14:21:10 +0900

    feat : 유저 정보 조회 (#26)
    * feat : securitycontext 이용
    
    * fix : profile 링크 속성 도메인에 추가
    
    * feat : member 정보 조회

---
+++ src/main/java/com/pickkasso/global/util/SecurityUtil.java
@@ -0,0 +1,21 @@
+package com.pickkasso.global.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import com.pickkasso.global.error.exception.CustomException;
+import com.pickkasso.global.error.exception.ErrorCode;
+
+@Component
+public class SecurityUtil {
+
+    public Long getCurrentMemberId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        try {
+            return Long.parseLong(authentication.getName());
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.AUTH_NOT_FOUND);
+        }
+    }
+}
