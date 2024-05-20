29703f5c89cd7abe148f490453ded00e34bf3c92
Jisun You <154600308+iiqcov@users.noreply.github.com>
2024-05-20 10:46:30 +0900

    feat : 유저 회원탈퇴 구현 (#41)

--- src/main/java/com/pickkasso/domain/member/api/MemberController.java
+++ src/main/java/com/pickkasso/domain/member/api/MemberController.java
@@ -1,11 +1,15 @@
 package com.pickkasso.domain.member.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pickkasso.domain.member.application.MemberService;
 import com.pickkasso.domain.member.dto.response.MemberInfoResponse;
+import com.pickkasso.global.util.CookieUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,9 +18,18 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/users")
 public class MemberController {
     private final MemberService memberService;
+    private final CookieUtil cookieUtil;
 
     @GetMapping
     public MemberInfoResponse getMemberProfile() {
         return memberService.getMemberInfo();
+    }
+
+    @DeleteMapping("/withdrawal")
+    public ResponseEntity<Void> memberWithdrawal() {
+        memberService.withdrawal();
+        return ResponseEntity.status(HttpStatus.OK)
+                .headers(cookieUtil.deleteTokenCookies())
+                .build();
     }
 }
--- src/main/java/com/pickkasso/domain/member/application/MemberService.java
+++ src/main/java/com/pickkasso/domain/member/application/MemberService.java
@@ -2,6 +2,7 @@ package com.pickkasso.domain.member.application;
 
 import org.springframework.stereotype.Service;
 
+import com.pickkasso.domain.auth.dao.RefreshTokenRepository;
 import com.pickkasso.domain.member.domain.Member;
 import com.pickkasso.domain.member.dto.response.MemberInfoResponse;
 import com.pickkasso.global.util.MemberUtil;
@@ -12,9 +13,15 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class MemberService {
     private final MemberUtil memberUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public MemberInfoResponse getMemberInfo() {
         final Member member = memberUtil.getCurrentMember();
         return new MemberInfoResponse(member.getNickname(), member.getOauthInfo().getProfile());
+    }
+
+    public void withdrawal() {
+        final Member member = memberUtil.getCurrentMember();
+        refreshTokenRepository.deleteById(member.getId());
     }
 }
