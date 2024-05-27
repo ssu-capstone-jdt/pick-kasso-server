291a691ded7c8710bb9b4199c3117bfd8411e6d7
Nevaeh0701 <Eunsoo071@gmail.com>
2024-05-27 14:34:14 +0900

    fix : painting 도메인 수정

--- src/main/java/com/pickkasso/domain/painting/domain/Painting.java
+++ src/main/java/com/pickkasso/domain/painting/domain/Painting.java
@@ -1,41 +1,17 @@
 // Painting
 package com.pickkasso.domain.painting.domain;
 
-import java.time.LocalDateTime;
 
+import com.pickkasso.domain.common.model.BaseEntity;
 import jakarta.persistence.*;
 
 import lombok.*;
 
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Painting {
-    //    @Id
-    //    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //    @Column(name = "id", updatable = false)
-    //    private Long id;
-    //
-    //    @Column(name = "user_id", nullable = false)
-    //    private String userId;
-    //
-    //    @Column(name = "painting_link", nullable = false)
-    //    private String paintingLink;
-    //
-    //    @Column(name = "painting_title")
-    //    private String paintingTitle;
-    //
-    //    @Column(name = "painting_state")
-    //    private boolean paintingState;
-    //
-    //    @Builder
-    //    public Painting(
-    //            String userId, String paintingLink, String paintingTitle, boolean paintingState) {
-    //        this.userId = userId;
-    //        this.paintingLink = paintingLink;
-    //        this.paintingTitle = paintingTitle;
-    //        this.paintingState = paintingState;
-    //    }
+public class Painting extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
@@ -59,9 +35,6 @@ public class Painting {
     @Column(name = "painting_Name")
     private String paintingName;
 
-    // 추가
-    @Column(name = "created_at")
-    private LocalDateTime timeStamp;
 
     @Builder
     public Painting(
@@ -87,10 +60,5 @@ public class Painting {
 
     public void setPaintingName(String paintingName) {
         this.paintingName = paintingName;
-    }
-
-    // 추가
-    public void setTimeStamp(LocalDateTime timestamp) {
-        this.timeStamp = timestamp;
     }
 }
